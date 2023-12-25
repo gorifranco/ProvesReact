ade4a1046cb8de1ca54a7254ed7d822d504fee59
gorifranco <gorifranco@gmail.com>
2023-12-25 21:07:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header/Header.jsx";
+
+export function App () {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+	return (
+<div>Header</div>
+	);
+};
+
+export default Header;
